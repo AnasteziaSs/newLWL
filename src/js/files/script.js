5ace0ee5d5132289d493caa4304a8303179ffd98
AnasteziaS <nikola.spamzzz@gmail.com>
2021-12-20 20:34:04 +0600

    js-click_on_menu>submenu

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -1,3 +1,11 @@
 // Импорт функционала ==============================================================================================================================================================================================================================================================================================================================
 // import { isMobile } from "./functions.js";
 // import { formsModules } from "./forms/forms.js";
+
+
+let block = document.querySelector('.products');
+block.addEventListener('click', () => {
+	block.classList.toggle('_ok')
+})
+let submenu = document.querySelectorAll('.submenu');
+
