d85b3e20e69f3a8d69030fef280ec6026a115279
AnasteziaS <nikola.spamzzz@gmail.com>
2021-12-21 17:50:33 +0600

    js+header-flex

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -3,9 +3,31 @@
 // import { formsModules } from "./forms/forms.js";
 
 
-let block = document.querySelector('.products');
+/* let block = document.querySelector('.products');
 block.addEventListener('click', () => {
 	block.classList.toggle('_ok')
 })
 let submenu = document.querySelectorAll('.submenu');
-
+*/
+/* let block = document.querySelectorAll('.menu__link');
+console.log(block);
+for (let index = 0; index < block.length; index++) {
+	const element = block[index];
+	const preBlock = element.parentElement;
+	console.log(preBlock);
+	element.addEventListener('click', () => {
+		preBlock.classList.toggle('_ok')
+	})
+}
+*/
+let wrap = document.querySelector('.wrapper');
+let block = document.querySelectorAll('.menu__link');
+console.log(block);
+for (let index = 0; index < block.length; index++) {
+	const element = block[index];
+	const preBlock = element.parentElement;
+	//console.log(preBlock);
+	element.addEventListener('click', () => {
+		preBlock.classList.toggle('_ok')
+	})
+}
