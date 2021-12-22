492169dbbe5f2d496cf7ad95f93e68d557652bee
AnasteziaS <nikola.spamzzz@gmail.com>
2021-12-22 21:18:12 +0600

    fn-submenu

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -20,7 +20,7 @@ for (let index = 0; index < block.length; index++) {
 	})
 }
 */
-let wrap = document.querySelector('.wrapper');
+/*let wrap = document.querySelector('.wrapper');
 let block = document.querySelectorAll('.menu__link');
 console.log(block);
 for (let index = 0; index < block.length; index++) {
@@ -31,3 +31,30 @@ for (let index = 0; index < block.length; index++) {
 		preBlock.classList.toggle('_ok')
 	})
 }
+*/
+//========================================================================================================================================================
+let block = document.querySelector('.menu__item');
+document.addEventListener('click', documentActions, closeMenu);
+
+function documentActions(e) {
+	let targetElement = e.target;
+	let targetPrevItem = targetElement.parentElement;
+	let blockOk = document.querySelector('._ok');
+	if (targetElement !== blockOk && blockOk !== null && !targetElement.closest('.menu')) {
+		document.querySelector('._ok').classList.remove('_ok');
+	}
+	if (targetElement.closest('.menu__item')) {
+		targetPrevItem.classList.toggle('_ok');
+	}
+}
+function closeMenu(e) {
+
+}
+
+//========================================================================================================================================================
+
+let search = document.querySelector('.actions-header__search');
+let form = document.querySelector('.header__form');
+search.addEventListener('click', () => {
+	form.classList.toggle("_ok");
+})
